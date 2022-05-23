7e6e402504c620e62d31e943d7d8998c309e0329
Dragonmaster129 <dragons4756@gmail.com>
2022-05-23 12:06:37 -0600

    Payloan can no longer go above your debt

--- src/components/actionsResults/payloan.js
+++ src/components/actionsResults/payloan.js
@@ -2,31 +2,26 @@ import React, { useState, useEffect } from "react";
 
 const PayLoan = (props) => {
   const [loanAmount, setloanAmount] = useState(1000);
-  const [canPayThat, setcanPayThat] = useState(false);
 
   useEffect(() => {
-    if (props.data.expenses.loan >= loanAmount) {
-      setcanPayThat(true);
-    } else {
-      setcanPayThat(false);
+    if (props.data.expenses.loan < loanAmount) {
+      setloanAmount(loanAmount - 1000);
     }
   }, [loanAmount]);
 
   const onSubmit = (event) => {
     event.preventDefault();
-    if (canPayThat) {
-      let cData = props.data;
-      let newData = {};
-      for (const key in cData) {
-        if (Object.hasOwnProperty.call(cData, key)) {
-          const element = cData[key];
-          newData[key] = element;
-        }
+    let cData = props.data;
+    let newData = {};
+    for (const key in cData) {
+      if (Object.hasOwnProperty.call(cData, key)) {
+        const element = cData[key];
+        newData[key] = element;
       }
-      newData.expenses.loan = newData.expenses.loan - loanAmount;
-      props.setdata(newData);
-      props.setcash(props.cash - loanAmount);
     }
+    newData.expenses.loan = newData.expenses.loan - loanAmount;
+    props.setdata(newData);
+    props.setcash(props.cash - loanAmount);
     props.setborrowLoan("NONE");
   };
   return (
@@ -49,7 +44,6 @@ const PayLoan = (props) => {
           }}
         ></input>
         <label>Will increase income by: {loanAmount / 10}</label>
-        <label>{canPayThat ? "" : "Your bank loan is not that large!"}</label>
         <button onClick={onSubmit} className="pay-loan">
           DO IT!
         </button>
