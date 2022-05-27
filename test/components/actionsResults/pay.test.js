153f548dae860b871d2ba9f4ad1e05f889ad31c9
Dragonmaster129 <dragons4756@gmail.com>
2022-05-27 11:05:12 -0600

    Created a test for paying money

---
+++ test/components/actionsResults/pay.test.js
@@ -0,0 +1,21 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import React from "react";
+import "@testing-library/jest-dom";
+import Pay from "../../../src/components/actionsResults/pay";
+
+let cash = 3950;
+let setcash = (value) => (cash = value);
+let onChange = (value) => {};
+
+test("renders", () => {
+  render(<Pay cash={cash} setcash={setcash} submitted={onChange} />);
+  let pay = screen.getByRole("button");
+  expect(pay).toBeDefined();
+});
+
+test("clicking will decrease your cash", () => {
+  render(<Pay cash={cash} setcash={setcash} submitted={onChange} />);
+  let pay = screen.getByRole("button");
+  fireEvent.click(pay);
+  expect(cash).toEqual(3850);
+});
