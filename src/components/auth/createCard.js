f5987120c18913c31a9399865c61ec196dd940a0
Dragonmaster129 <dragons4756@gmail.com>
2022-07-27 14:12:24 -0600

    Cleaned up the doodad code, added market fields, and royalty

--- src/components/auth/createCard.js
+++ src/components/auth/createCard.js
@@ -3,21 +3,15 @@ import React, { useState, useEffect } from "react";
 import { SERVER_HOST } from "../constants";
 
 const CreateCard = (props) => {
-  const [cardType, setcardType] = useState("capitalgain");
-  const [cashflow, setcashflow] = useState(0);
-  const [cash, setcash] = useState(0);
-  const [baby, setbaby] = useState(false);
-  const [title, settitle] = useState("");
-  const [cashflowType, setcashflowType] = useState("other");
-  const [description, setdescription] = useState("");
+  const [cardType, setcardType] = useState("market");
   const [stockOption, setstockOption] = useState("regular");
   const [d2yOption, setd2yOption] = useState("card1");
+  const [targetPlayer, settargetPlayer] = useState("player");
 
   const [objectToSend, setobjectToSend] = useState({ title: "" });
   const [typeDropdown, settypeDropdown] = useState("realestate");
   const [currentMode, setcurrentMode] = useState("create");
 
-  const [result, setresult] = useState("");
   const [cardList, setcardList] = useState({
     beginning: [],
     capitalgain: [],
@@ -40,13 +34,6 @@ const CreateCard = (props) => {
       description: "",
       type: "",
       name: "",
-      highest: true,
-      price: 0,
-      bankrupt: true,
-      size: 0,
-      value: 0,
-      property: {},
-      forcedSale: true,
       target: "",
     },
 
@@ -78,12 +65,8 @@ const CreateCard = (props) => {
   });
 
   useEffect(() => {
-    setresult("");
-  }, [cardType, cashflow, cash, baby, cashflowType, description]);
-
-  useEffect(() => {
-    settypeDropdown("realestate");
     setobjectToSend(format[cardType]);
+    settypeDropdown("realestate");
   }, [cardType]);
 
   useEffect(() => {
@@ -93,8 +76,9 @@ const CreateCard = (props) => {
         object[key] = objectToSend[key];
       }
       if (cardType === "capitalgain") {
+        console.log(typeDropdown);
         object["card"] = { type: "", name: "" };
-        if (typeDropdown === "realestate") {
+        if (typeDropdown === "realestate" || typeDropdown === "land") {
           object["card"]["size"] = 1;
           object["card"]["cost"] = 0;
           object["card"]["mortgage"] = 0;
@@ -103,10 +87,14 @@ const CreateCard = (props) => {
         } else if (typeDropdown === "stock") {
           object["card"]["option"] = "";
           object["card"]["costPerShare"] = 0;
+          if (stockOption === "regular") {
+            object["card"]["target"] = "player";
+          }
           if (stockOption === "call" || stockOption === "put") {
             object["card"]["strikePrice"] = 0;
           }
         } else if (typeDropdown === "d2y") {
+          object["card"]["name"] = "card1";
           object["card"]["cost"] = 200;
           object["card"]["downpay"] = 200;
         }
@@ -122,7 +110,53 @@ const CreateCard = (props) => {
           object["card"]["name"] = "card1";
           object["card"]["cost"] = 200;
           object["card"]["downpay"] = 200;
+        } else if (typeDropdown === "royalty") {
+          object["card"]["cost"] = 12000;
+          object["card"]["downpay"] = 12000;
+          object["card"]["value"] = 400;
         }
+      } else if (cardType === "market") {
+        object = {};
+        for (const key in format[cardType]) {
+          object[key] = objectToSend[key];
+        }
+        if (typeDropdown === "realestate") {
+          object["highest"] = false;
+          object["size"] = 1;
+          object["price"] = 0;
+          object["forcedSale"] = false;
+        } else if (typeDropdown === "realestate Exchange") {
+          object["newProperty"] = {
+            name: "",
+            size: 1,
+            cost: 0,
+            mortgage: 0,
+            downpay: 0,
+            value: 0,
+          };
+        } else if (typeDropdown === "stock") {
+          object["bankrupt"] = false;
+          object["price"] = 0;
+          object["forcedSale"] = false;
+        } else if (typeDropdown === "d2y") {
+          object["value"] = 7000;
+        } else if (typeDropdown === "land") {
+          object["size"] = 5;
+          object["price"] = 0;
+        }
+      } else if (cardType === "beginning") {
+        object["stock"] = {};
+        object["stock"]["name"] = "";
+        object["stock"]["option"] = "";
+        object["stock"]["amount"] = 0;
+        object["stock"]["costPerShare"] = 0;
+        object["realestate"] = {};
+        object["realestate"]["name"] = "";
+        object["realestate"]["size"] = 1;
+        object["realestate"]["cost"] = 0;
+        object["realestate"]["mortgage"] = 0;
+        object["realestate"]["downpay"] = 0;
+        object["realestate"]["value"] = 0;
       }
       setobjectToSend(object);
     }
@@ -132,7 +166,6 @@ const CreateCard = (props) => {
     if (cardType === "cashflow") {
       if (typeDropdown === "d2y") {
         let object = {};
-
         for (let key in objectToSend) {
           object[key] = objectToSend[key];
         }
@@ -159,27 +192,6 @@ const CreateCard = (props) => {
       })
       .catch((err) => console.log(err));
   }, []);
-
-  function doodadSubmit(event) {
-    event.preventDefault();
-    axios
-      .post(`${SERVER_HOST}/card/add/doodad`, {
-        cardType: cardType,
-        cashflow: cashflow,
-        cash: cash,
-        baby: baby,
-        cashflowType: cashflowType,
-        token: props.credentials,
-        title: title,
-        description: description,
-      })
-      .then((res) => {
-        setresult(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
 
   function HandleChange({ target }) {
     let smallObject = {};
@@ -211,33 +223,40 @@ const CreateCard = (props) => {
     );
   }
 
-  function generateDropdown(value, setter, options) {
+  function generateDropdown(value, setter, options, label) {
     return (
-      <select
-        key={value}
-        value={value}
-        onChange={(event) => {
-          setter(event.target.value);
-        }}
-      >
-        {options.map((optionValue) => {
-          return (
-            <option key={optionValue} value={optionValue}>
-              {optionValue}
-            </option>
-          );
-        })}
-      </select>
+      <div className="v" key={value}>
+        <label>{label}</label>
+        <select
+          value={value}
+          onChange={(event) => {
+            setter(event.target.value);
+          }}
+        >
+          {options.map((optionValue) => {
+            return (
+              <option key={optionValue} value={optionValue}>
+                {optionValue}
+              </option>
+            );
+          })}
+        </select>
+      </div>
     );
   }
 
   function ShowCreateCardChoices(types) {
     let keys = Object.keys(objectToSend);
     let mapping = keys.map((key) => {
+      if (key === "target" || key === "type") {
+        return null;
+      }
       if (typeof objectToSend[key] === "string") {
         return generateInput(key, "text", objectToSend[key]);
       } else if (typeof objectToSend[key] === "number") {
         return generateInput(key, "number", objectToSend[key]);
+      } else if (typeof objectToSend[key] === "boolean") {
+        return generateInput(key, "checkbox", objectToSend[key]);
       } else if (typeof objectToSend[key] === "object") {
         return Object.keys(objectToSend[key]).map((field) => {
           if (field === "type") {
@@ -250,31 +269,55 @@ const CreateCard = (props) => {
             cardType === "cashflow"
           ) {
             return null;
+          } else if (field === "target") {
+            return generateDropdown(
+              targetPlayer,
+              settargetPlayer,
+              ["you", "right", "all"],
+              "Target Player"
+            );
           }
-          return generateInput(
-            key + "." + field,
-            "text",
-            objectToSend[key][field]
-          );
+          if (typeof objectToSend[key][field] === "string") {
+            return generateInput(
+              key + "." + field,
+              "text",
+              objectToSend[key][field]
+            );
+          } else if (typeof objectToSend[key][field] === "number") {
+            return generateInput(
+              key + "." + field,
+              "number",
+              objectToSend[key][field]
+            );
+          }
         });
       }
     });
-    if (typeDropdown === "stock") {
+    if (typeDropdown === "stock" && cardType !== "market") {
       mapping.unshift(
-        generateDropdown(stockOption, setstockOption, [
-          "regular",
-          "call",
-          "put",
-          "short",
-        ])
+        generateDropdown(
+          stockOption,
+          setstockOption,
+          ["regular", "call", "put", "short"],
+          "stock type"
+        )
       );
     }
     if (typeDropdown === "d2y" && cardType === "cashflow") {
       mapping.unshift(
-        generateDropdown(d2yOption, setd2yOption, ["card1", "card2", "card3"])
+        generateDropdown(
+          d2yOption,
+          setd2yOption,
+          ["card1", "card2", "card3"],
+          "d2y Option"
+        )
       );
     }
-    mapping.unshift(generateDropdown(typeDropdown, settypeDropdown, types));
+    if (types.length !== 0) {
+      mapping.unshift(
+        generateDropdown(typeDropdown, settypeDropdown, types, "type of card")
+      );
+    }
     return mapping;
   }
 
@@ -309,69 +352,6 @@ const CreateCard = (props) => {
         </div>
       );
     });
-  }
-
-  function doodad() {
-    return (
-      <div className="v">
-        <label>CashFlow</label>
-        <input
-          type="number"
-          value={cashflow}
-          onChange={(event) => setcashflow(event.target.value)}
-        ></input>
-        {cashflow > 0 ? (
-          <div>
-            <select
-              value={cashflowType}
-              onChange={(event) => {
-                setcashflowType(event.target.value);
-              }}
-            >
-              <option value="taxes">Taxes</option>
-              <option value="mortgage">Mortgage</option>
-              <option value="school">School</option>
-              <option value="car">Car</option>
-              <option value="creditCard">CreditCard</option>
-              <option value="retail">Retail</option>
-              <option value="other">Other</option>
-              <option value="child">Child</option>
-              <option value="loan">Loan</option>
-            </select>
-          </div>
-        ) : (
-          ""
-        )}
-        <label>Cash</label>
-        <input
-          type="number"
-          value={cash}
-          onChange={(event) => setcash(event.target.value)}
-        ></input>
-        <div className="hz">
-          <input
-            type="checkbox"
-            checked={baby}
-            onChange={(event) => setbaby(!baby)}
-          ></input>
-          <label>Needs Baby?</label>
-        </div>
-        <label>Title</label>
-        <input
-          type="text"
-          value={title}
-          onChange={(event) => settitle(event.target.value)}
-        ></input>
-        <label>Description</label>
-        <textarea
-          value={description}
-          onChange={(event) => {
-            setdescription(event.target.value);
-          }}
-        ></textarea>
-        <button onClick={doodadSubmit}>Submit</button>
-      </div>
-    );
   }
 
   function mapAllValuesAndKeys(card) {
@@ -413,7 +393,12 @@ const CreateCard = (props) => {
     <div className="create-card-wrapper">
       <div className="card-form-wrapper">
         <h1>CreateCard</h1>
-        <form>
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+            console.log("Submitted!");
+          }}
+        >
           <select
             value={cardType}
             onChange={(event) => {
@@ -426,25 +411,25 @@ const CreateCard = (props) => {
             <option value="doodad">Doodad</option>
             <option value="market">Market</option>
           </select>
-          {cardType == "beginning" ? ShowCreateCardChoices() : ""}
+          {cardType == "beginning" ? ShowCreateCardChoices([]) : ""}
           {cardType == "capitalgain"
-            ? ShowCreateCardChoices(["stock", "realestate", "d2y"])
+            ? ShowCreateCardChoices(["stock", "realestate", "d2y", "land"])
             : ""}
           {cardType == "cashflow"
-            ? ShowCreateCardChoices(["stock", "realestate", "d2y"])
+            ? ShowCreateCardChoices(["realestate", "d2y", "royalty"])
             : ""}
-          {cardType == "doodad" ? doodad() : ""}
+          {cardType == "doodad" ? ShowCreateCardChoices([]) : ""}
           {cardType == "market"
-            ? ShowCreateCardChoices(["stock", "realestate"])
+            ? ShowCreateCardChoices([
+                "stock",
+                "realestate",
+                "realestate Exchange",
+                "d2y",
+                "land",
+              ])
             : ""}
+          <button type="submit">Submit</button>
         </form>
-        {result == "" ? (
-          ""
-        ) : result ? (
-          <div>Card Submitted</div>
-        ) : (
-          <div>Card Failed to submit, try logging in</div>
-        )}
       </div>
       <div className="card-wrapper">
         <div>
