b8750bf1fffadbe4ac6f10e47085273bb714897a
Dragonmaster129 <dragons4756@gmail.com>
2022-07-29 10:25:54 -0600

    Added styles to the error page.

--- src/components/pages/errorPage.js
+++ src/components/pages/errorPage.js
@@ -3,14 +3,17 @@ import { Link } from "react-router-dom";
 
 const ErrorPage = (props) => {
   return (
-    <div>
-      <h1>Uh Oh. There's nothing here.</h1>
-      <h1>Click on the link below to login.</h1>
-      <h1>
-        <Link to="login" className="link">
-          Login
-        </Link>
-      </h1>
+    <div className="error-page-wrapper">
+      <div className="error-page">
+        <h1>Uh Oh. There's nothing here.</h1>
+        <h1>Sorry about that.</h1>
+        <h1>Click on the link below to login.</h1>
+        <h1>
+          <Link to="login" className="link">
+            Login
+          </Link>
+        </h1>
+      </div>
     </div>
   );
 };
